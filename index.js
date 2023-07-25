29c04e9ce3ee13b0ee3090365f088597abcd6b50
Kamal Kumar <iamkamalkumar17@gmail.com>
2023-07-26 02:55:00 +0530

    completely working backend

--- index.js
+++ index.js
@@ -2,35 +2,52 @@ import express from "express";
 import bodyParser from "body-parser";
 
 const app = express();
+const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const port = 3000;
+//set the static folder to public
 app.use(express.static("public"));
 
-var todayList=["first"];
-var workList=["first"];
+var todayList=["First of the day"];
+var workList=["Got to work"];
+
+const date = new Date();
+const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+var toPrint = weekday[date.getDay()] + ", " +month[date.getMonth()] + " " + date.getDate()
+
+
 app.post("/today", (req, res)=>{
     var newTask= req.body["nextTaskToday"];
     console.log("today: " +newTask);
     todayList.push(newTask);
-    console.log(todayList);
-    res.render("today.ejs");
+    res.render("today.ejs", {
+        list: todayList,
+        head: toPrint
+    });
 })
 
 app.post("/work", (req, res)=>{
     var newTask = req.body["nextTaskWork"];
     console.log("work: " + newTask);
     workList.push(newTask);
-    console.log(workList);
+    res.send("work.ejs", {
+        list: workList
+    })
 
 })
 
 app.get("/work", (req, res)=>{
-    res.render("work.ejs");
+    res.render("work.ejs", {
+        list: workList
+    });
 })
 app.get("/", (req, res)=>{
-    res.render("today.ejs")
+    res.render("today.ejs", {
+        list: todayList,
+        head: toPrint
+    })
 })
 app.listen(port, ()=>{
     console.log(`app runs at ${port}`);
