26c5f8da61163cacf9576f81937aac5218587413
Kamal Kumar <iamkamalkumar17@gmail.com>
2023-07-26 02:10:10 +0530

    a little bit on backend

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 import express from "express";
+import bodyParser from "body-parser";
+
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const port = 3000;
 app.use(express.static("public"));
+
+var todayList=["first"];
+var workList=["first"];
+app.post("/today", (req, res)=>{
+    var newTask= req.body["nextTaskToday"];
+    console.log("today: " +newTask);
+    todayList.push(newTask);
+    console.log(todayList);
+    res.render("today.ejs");
+})
+
+app.post("/work", (req, res)=>{
+    var newTask = req.body["nextTaskWork"];
+    console.log("work: " + newTask);
+    workList.push(newTask);
+    console.log(workList);
+
+})
 
 app.get("/work", (req, res)=>{
     res.render("work.ejs");
