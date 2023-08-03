f8635d1bf790ddcada6d4e0da59233927c8cb1c5
Kamal Kumar <iamkamalkumar17@gmail.com>
2023-08-03 12:02:02 +0530

    working copy with work list

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-import express from "express";
-import bodyParser from "body-parser";
+// import express from "express";
+// import bodyParser from "body-parser";
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 const port = 3000;
@@ -9,65 +12,81 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //set the static folder to public
 app.use(express.static("public"));
 
-var todayList = ["First of the day"];
-var workList = ["Got to work"];
+mongoose.connect("mongodb://127.0.0.1:27017/todolistDB").then(() => {
+  console.log("mongodb connected at port 27017");
+});
 
-const date = new Date();
-const weekday = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-const month = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
-var toPrint =
-  weekday[date.getDay()] + ", " + month[date.getMonth()] + " " + date.getDate();
+const itemSchema = new mongoose.Schema({
+  name: String
+});
+const Item = mongoose.model("Item", itemSchema);
+
+const listSchema = new mongoose.Schema({
+  name: String,
+  items: [itemSchema]
+});
+const List = mongoose.model("List", listSchema);
+
+
+const item1 = new Item({
+  name: "Welcome to your todo list"
+})
+const item2 = new Item({
+  name : "yours completely"
+});
+const defaultItems = [item1, item2];  
+// Item.insertMany(defaultItems).then(() => {console.log("inserted")});
+
+
 
 app.post("/today", (req, res) => {
-  var newTask = req.body["nextTaskToday"];
-  console.log("today: " + newTask);
-  if (newTask.length > 0) todayList.push(newTask);
-  res.render("today.ejs", {
-    list: todayList,
-    head: toPrint,
-  });
+  const itemName = req.body["nextTaskToday"];
+  const newItem = new Item({ name: itemName});
+  newItem.save();
+  res.redirect("/");
+
 });
 
-app.post("/work", (req, res) => {
-  var newTask = req.body["nextTaskWork"];
-  console.log("work: " + newTask);
-  if (newTask.length > 0) workList.push(newTask);
-  res.render("work.ejs", {
-    list: workList,
-  });
-});
+app.post("/delete", (req, res)=>{
+  const removedItemId= req.body.checkbox;
+  Item.deleteOne({_id: removedItemId}).then(function(){
+    console.log("deleted");
+  
+  })
+  res.redirect("/");
+})
 
-app.get("/work", (req, res) => {
-  res.render("work.ejs", {
-    list: workList,
-  });
-});
+
+app.get("/:customListName", (req, res) => {
+  const customListName = req.params.customListName;
+
+  List.findOne({name: customListName}).then((data) => {
+    if(data.length === 0) {
+      const list = new List({
+        name: customListName,
+        items: defaultItems
+      });
+      list.save();
+      res.redirect("/:" + customListName);
+    }
+    else {
+      res.render("today.ejs", { head: data.name, list: data.items})
+    }
+  })
+  
+})
+
+
 app.get("/", (req, res) => {
-  res.render("today.ejs", {
-    list: todayList,
-    head: toPrint,
-  });
+  Item.find().then((foundItems)=>{
+    if(foundItems.length === 0) {
+      Item.insertMany(defaultItems).then(() => {console.log("inserted")});
+      res.redirect("/");
+    }
+    else {
+      res.render("today.ejs", {head: "today", list: foundItems});
+    }
+  })
 });
 app.listen(port, () => {
   console.log(`app runs at ${port}`);
